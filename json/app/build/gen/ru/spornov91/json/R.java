e05d4f3ff0a48b04acebd770ae56b0b0304bee87
spornov91 <m79513829426@gmail.com>
2022-12-05 13:27:02 +0700

    update json add all user repos

--- json/app/build/gen/ru/spornov91/json/R.java
+++ json/app/build/gen/ru/spornov91/json/R.java
@@ -15,8 +15,7 @@ public final class R {
     }
     public static final class id {
         public static final int btnGetUserGithub=0x7f070000;
-        public static final int lvUserGithub=0x7f070002;
-        public static final int mainTextView=0x7f070001;
+        public static final int lvUserGithub=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- json/app/src/main/java/ru/spornov91/json/MainActivity.java
+++ json/app/src/main/java/ru/spornov91/json/MainActivity.java
@@ -8,10 +8,12 @@ import java.io.*;
 import android.util.*;
 import android.view.*;
 import org.json.*;
+import java.util.*;
 
 public class MainActivity extends Activity 
 {
 	String TAG = "spornov91";
+	String[] names;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,91 +23,90 @@ public class MainActivity extends Activity
 		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         
-		String[] names = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
-			"Костя", "Игорь", "Анна", "Денис", "Андрей" };
-		
-		ListView lvMain = findViewById(R.id.lvUserGithub);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-		android.R.layout.simple_list_item_1, names);
-		lvMain.setAdapter(adapter);
-		
 		final TextView tvJsonData = findViewById(R.id.mainTextView);
 		
 		Button getUserGithub = findViewById(R.id.btnGetUserGithub);
 		getUserGithub.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View arg0) {
-					new AsyncRequest().execute();
 					
+					Thread t = new Thread(new Runnable() { 
+							public void run() {
+							
+								String url = "https://api.github.com/users/spornov91/repos";
+								HttpURLConnection c = null;
+								try {
+									URL u = new URL(url);
+									c = (HttpURLConnection) u.openConnection();
+									c.setRequestMethod("GET");
+									c.setRequestProperty("Content-length", "0");
+									c.setUseCaches(false);
+									c.setAllowUserInteraction(false);
+									c.connect();
+									int status = c.getResponseCode();
+									Log.d("status",""+status);
+									switch (status) {
+										case 200:
+										case 201:
+											BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
+											StringBuilder sb = new StringBuilder();
+											String line;
+											while ((line = br.readLine()) != null) {
+												sb.append(line+"\n");
+											}
+											br.close();
+											names = jsontoarr(sb.toString());
+									}
+								} catch (MalformedURLException ex) {
+
+								} catch (IOException ex) {
+
+								} finally {
+									if (c != null) {
+										try {
+											c.disconnect();
+											
+											new Handler(getMainLooper()).post(
+												new Runnable() {
+													@Override
+													public void run () {
+														ListView lvMain = findViewById(R.id.lvUserGithub);
+														ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
+																												android.R.layout.simple_list_item_1, names);
+														lvMain.setAdapter(adapter);
+													}
+												});
+										} catch (Exception ex) {
+
+										}
+									}
+								}
+							};
+						});
+					t.start();
 				}
 			});
 			
-		
-			
     }
 	
-	class AsyncRequest extends AsyncTask<String, Integer, String>{
-
-		@Override
-		protected String doInBackground(String... arg) {
-
-			//int timeout = 15000;
-			//String url = "https://127.0.0.1:2012/realjson";
-			String url = "https://api.github.com/users/spornov91";
-			HttpURLConnection c = null;
-			try {
-				URL u = new URL(url);
-				c = (HttpURLConnection) u.openConnection();
-				c.setRequestMethod("GET");
-				c.setRequestProperty("Content-length", "0");
-				c.setUseCaches(false);
-				c.setAllowUserInteraction(false);
-				//c.setConnectTimeout(timeout);
-				//c.setReadTimeout(timeout);
-				c.connect();
-				int status = c.getResponseCode();
-				Log.d("status",""+status);
-				switch (status) {
-					case 200:
-					case 201:
-						BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
-						StringBuilder sb = new StringBuilder();
-						String line;
-						while ((line = br.readLine()) != null) {
-							sb.append(line+"\n");
-						}
-						br.close();
-						try{
-						    JSONObject obj = new JSONObject(sb.toString());
-							//JSONArray arr = obj.getJSONArray("Prod");
-							String _name = obj.getString("name");
-							Log.d("obj.name",_name);
-						}catch (JSONException e) {
-							e.printStackTrace();
-						}
-						//Log.d("br.readLine",sb.toString());
-						return sb.toString();
-				}
-			} catch (MalformedURLException ex) {
-
-			} catch (IOException ex) {
-
-			} finally {
-				if (c != null) {
-					try {
-						c.disconnect();
-					} catch (Exception ex) {
-
-					}
-				}
-			}
-			return null;
+	private static String[] jsontoarr(String str)
+	{
+		ArrayList<String> list = new ArrayList<String>();
+		try{
+		//JSONObject obj = new JSONObject(str);
+		JSONArray arr = new JSONArray(str);
+		for (int n = 0; n < arr.length(); n++) {
+			JSONObject obj = arr.getJSONObject(n);
+			//JSONArray arr = obj.getJSONArray("Prod");
+			String _name = obj.getString("name");
+			Log.d("obj.name",_name);
+			list.add(_name);
+		} 
+		
+		}catch (JSONException e) {
+			e.printStackTrace();
 		}
-
-		@Override
-		protected void onPostExecute(String s) {
-			super.onPostExecute(s);
-			//tvJsonData.setText(s);
-		}
-	}
+		
+		return list.toArray(new String[0]);
+    };
 }
